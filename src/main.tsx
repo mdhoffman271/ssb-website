62927414a7dcc7622c02f22b60c79ab5a43f8a00
mdhoffman271 <mdhoffman271@gmail.com>
2026-03-27 20:13:29 +0000

    Fixes for github pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ssb-website">
+      {/* TODO remove basename later */}
       <App />
     </BrowserRouter>
   </StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 
 export default defineConfig({
+  base: '/ssb-website/', // TODO remove later
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
